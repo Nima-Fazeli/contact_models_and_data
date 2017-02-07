65d02c2676d7d665d5005ee19bbc20317258e269
nfazeli <nimfazeli@gmail.com>
2017-02-07 17:45:04 -0500

    update to visualizer of mu and epsilon;

--- data_inspection/visualizeMuEpsilon.m
+++ data_inspection/visualizeMuEpsilon.m
@@ -4,6 +4,10 @@ close all
 addpath('/Users/nimafazeli/Documents/MATLAB/2017 01 - Learning Contact New Data/models')
 addpath('/Users/nimafazeli/Documents/MATLAB/2017 01 - Learning Contact New Data/data')
 load('epsilon_vy_APNewton')
+% load('epsilon_vy_APPoisson')
+% load('epsilon_vy_mirtich')
+% load('epsilon_vy_wang')
+% load('epsilon_vy_DrumShell')
 
 ind_to_plot = find(data_vec(:,1)>-0.5);
 figure
@@ -26,6 +30,16 @@ plot(-data_vec(ind_to_plot,6),data_vec(ind_to_plot,2),'o')
 xlabel 'Change in Linear Momentum Y'
 ylabel 'Coeff of Restitution'
 
-
+figure
+subplot(211)
+ksdensity(data_vec(ind_to_plot,1))
+ylabel('PDE of Mu')
+xlabel('Mu Values')
+grid on
+subplot(212)
+ksdensity(data_vec(ind_to_plot,2))
+ylabel('PDE of Epsilon')
+xlabel('Epsilon Values')
+grid on
 end
 
